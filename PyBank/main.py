e0fdc7e37a5ddaae824549dbaa9bc5990717552b
Sunil Agarwal <sunil.agarwal@gmail.com>
2019-09-03 14:03:21 -0700

    Python Challange Solution

---
+++ PyBank/main.py
@@ -0,0 +1,62 @@
+import csv
+import os
+
+#print("****************************")
+#print("current path: " + os.getcwd())
+
+# Set path for file
+budget_csv = os.path.join(".", "Resources", "budget_data.csv")
+
+#open and read cvs
+with open(budget_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    # This is to skip the header in the csv file
+    next(csvreader)
+
+    #Creating empty list
+    date = []
+    revenue = []
+    revenue_change = []
+
+
+    # Loop through looking for the video
+    for row in csvreader:
+        date.append(row[0])
+        revenue.append(int(row[1]))
+
+    print("Financial Analysis")
+    print("----------------------------")
+    print("Total Months:", len(date))
+    print("Total: $",sum(revenue) )
+
+    for i in range(1, len(revenue)):
+        revenue_change.append(revenue[i] - revenue[i-1])
+        avg_revenue_change = sum(revenue_change)/len(revenue_change)
+
+        max_revenue_change =  max(revenue_change)
+        min_revenue_change = min(revenue_change)
+
+        date_for_max_revenue_change = str(date[revenue_change.index(max_revenue_change)+1])
+        date_for_min_revenue_change = str(date[revenue_change.index(min_revenue_change)+1])
+
+    print("Average Change: $",float(round(avg_revenue_change,2)))
+    print("Greatest Increase in Profits:",date_for_max_revenue_change,"($",int(max_revenue_change),")")
+    print("Greatest Decrease in Profits:",date_for_min_revenue_change,"($",int(min_revenue_change),")")
+
+    outputfile = open("budget_analysis.txt", "w")
+    outputfile.write("Financial Analysis \n")
+    outputfile.write("---------------------------- \n")
+
+    outputfile.write(f"Total Months: {len(date)}  \n")
+    outputfile.write(f"Total: ${sum(revenue)} \n")
+    outputfile.write(f"Average Change: ${float(round(avg_revenue_change,2))} \n")
+    outputfile.write(f"Greatest Increase in Profits: {date_for_max_revenue_change} (${int(max_revenue_change)})\n")
+    outputfile.write(f"Greatest Decrease in Profits: {date_for_min_revenue_change} (${int(min_revenue_change)})\n")
+
+
+
+
+
+
+
---
+++ PyPoll/main.py
@@ -0,0 +1,64 @@
+import csv
+import os
+
+# Function to perform election_data_analysis
+def election_data_analysis(candidate_name,candidate_list,total_votes,outputfile):
+    candidates_votes = 0
+    for candidate in candidate_list:
+        if candidate_name == candidate:
+            candidates_votes = candidates_votes + 1
+    win_percent = round((candidates_votes / total_votes) * 100,2)
+    print(f"{candidate_name}: {win_percent}% ({candidates_votes})")
+    outputfile.write(f"{candidate_name}: {win_percent}% ({candidates_votes})\n")
+    return candidates_votes
+
+# Set path for file
+election_data_csv = os.path.join(".", "Resources", "election_data.csv")
+
+#open and read cvs
+with open(election_data_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    # This is to skip the header in the csv file
+    header = next(csvreader)
+    #Creating empty list
+    voters = []
+    countries = []
+    candidate_list = []
+
+    # Loop through looking for the video
+    for row in csvreader:
+        voters.append(row[0])
+        countries.append(row[1])
+        candidate_list.append(row[2])
+
+    total_votes = len(voters)
+    print("Election Results")
+    print("----------------------------")
+    print("Total Votes:", total_votes )
+    print("----------------------------")
+
+    # Open the file using "write" mode. Specify the variable to hold the contents
+    outputfile = open("poll_analysis.txt", "w")
+    outputfile.write("Election Results \n")
+    outputfile.write("---------------------------- \n")
+    outputfile.write(f"Total Votes: {total_votes}  \n")
+    outputfile.write("---------------------------- \n")
+
+    candidates = set(candidate_list)
+    #print("Candidates List: $", candidates)
+    max_voting_count=0
+    winner = ""
+    for candidate_name in candidates:
+        #election_data_analysis(candidate_name,csvreader,total_votes)
+        candidates_votes = election_data_analysis(candidate_name, candidate_list, total_votes,outputfile)
+
+        if candidates_votes > max_voting_count:
+            max_voting_count = candidates_votes
+            winner = candidate_name
+    print("----------------------------")
+    print("Winner: ", winner)
+    print("----------------------------")
+
+    outputfile.write("----------------------------\n")
+    outputfile.write(f"Winner: {winner}\n")
+    outputfile.write("----------------------------")
